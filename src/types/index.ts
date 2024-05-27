a5c217b5aeb13ae52647a3c527c1999863467263
Melaynee <melayneeyt@gmail.com>
2024-05-27 17:31:33 +0200

    created types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+import { Chat, User, Message } from "@prisma/client";
+
+export type FullMessageType = Message & {
+  sender: User;
+  seen: User[];
+};
+
+export type FullChatType = Chat & {
+  users: User[];
+  messages: FullMessageType[];
+};
+
+
