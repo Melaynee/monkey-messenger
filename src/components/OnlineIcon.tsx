89e4e63c497437ee9e4a851f4d9c903f9839cd10
Melaynee <melayneeyt@gmail.com>
2024-05-27 13:12:10 +0200

    added online icon

---
+++ src/components/OnlineIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+// TODO add online status
+const OnlineIcon = (props: Props) => {
+  return (
+    <div className="w-3 h-3 bg-green-500 border-2 border-white rounded-full"></div>
+  );
+};
+
+export default OnlineIcon;
--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import { User } from "@prisma/client";
 import Image from "next/image";
 import React from "react";
+import OnlineIcon from "./OnlineIcon";
 
 type Props = { user?: User; size?: number };
 
@@ -9,17 +10,23 @@ const AvatarComponent: React.FC<Props> = ({ user, size }) => {
   const imageSrc = user?.image ?? "/img/avatar.jpeg";
   const imgSize = size ?? 40;
   return (
-    <Image
-      src={imageSrc}
-      alt="avatar"
-      width={imgSize}
-      height={imgSize}
-      className={cn(
-        "rounded-full overflow-hidden",
-        `w-${imgSize}px`,
-        `h-${imgSize}px`
-      )}
-    />
+    <div className="relative">
+      <Image
+        src={imageSrc}
+        alt="avatar"
+        width={imgSize}
+        height={imgSize}
+        className={cn(
+          "rounded-full overflow-hidden",
+          `w-${imgSize}px`,
+          `h-${imgSize}px`,
+          "cursor-pointer"
+        )}
+      />
+      <div className="absolute right-0 bottom-0">
+        <OnlineIcon />
+      </div>
+    </div>
   );
 };
 
