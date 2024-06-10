abd830512b9df9a43f3cdf368ba4bd3f7d9409f1
Melaynee <melayneeyt@gmail.com>
2024-06-10 11:39:16 +0200

    "Update Button component styles for secondary and main buttons"

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -23,7 +23,9 @@ const Button: React.FC<ButtonProps> = (props) => {
         "flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-white transition-colors duration-300",
         props.disabled && "opacity-50  cursor-not-allowed",
         props.fullWidth && "w-full",
-        props.secondary ? "bg-dark hover:bg-dark/90" : "bg-main",
+        props.secondary
+          ? "opacity-100 bg-white text-dark hover:opacity-80 transition-opacity duration-300"
+          : "bg-main",
         props.danger &&
           "bg-red-500 hover:bg-red-600 focus-visible:outline-red-600",
         !props.danger && !props.secondary && "bg-main hover:bg-hover"
