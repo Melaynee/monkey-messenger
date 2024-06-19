8f08ecbd92e215e6bab84d874a1ec04e75ef42d9
Melaynee <melayneeyt@gmail.com>
2024-06-19 15:54:16 +0200

    "Added tanstack react-query"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ToasterContext from "@/context/ToasterContext";
 import AuthContext from "@/context/AuthContext";
 import ActiveStatus from "@/components/ActiveStatus";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,9 +26,11 @@ export default function RootLayout({
       <body className={poppins.className}>
         <div className="h-screen">
           <AuthContext>
-            <ToasterContext />
-            <ActiveStatus />
-            {children}
+            <ReactQueryProvider>
+              <ToasterContext />
+              <ActiveStatus />
+              {children}
+            </ReactQueryProvider>
           </AuthContext>
         </div>
       </body>
